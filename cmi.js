8b0c4d74ebc5077369bf3405d2b6601fb2b288d6
Krunal1703 <119284280+Krunal1703@users.noreply.github.com>
2025-08-27 22:21:33 +0530

    Add files via upload

---
+++ cmi.js
@@ -0,0 +1,6 @@
+// Function to convert inches to cm
+function inchesToCm(inches) {
+    var cm = inches * 2.54;
+    document.getElementById("result").innerText = inches + " inches = " + cm + " cm";
+
+}
